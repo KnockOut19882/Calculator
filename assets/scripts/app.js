2b1af598e38ee0b20d3b7f4d4e5ea8e4dfaf6154
Mateusz <mpodolski1988@gmail.com>
2025-10-12 13:33:10 +0200

    add yarn server

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,9 +2,17 @@ const defaultResult = 0;
 
 let currentResult = defaultResult;
 
+function add(num1, num2) {
+  const result = num1 + num2;
+  alert('The result is: ' + result);
+}
+
+add(1, 2);
+add(5, 10);
+add (20, 30);
+
 currentResult = ((currentResult + 10) * 3) / 2 - 1;
 let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
 
-let errorMessage = "An error\v occurred";
 
 outputResult(currentResult, errorMessage);
