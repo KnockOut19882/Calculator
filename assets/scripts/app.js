e2ef17c73cf85c15bed0d100f766f06134ad90c7
Mateusz <mpodolski1988@gmail.com>
2025-10-13 10:47:21 +0200

    Add function

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,16 +3,10 @@ const defaultResult = 0;
 let currentResult = defaultResult;
 
 function add(num1, num2) {
-  const result = num1 + num2;
-  alert('The result is: ' + result);
+  currentResult += userInput.value; 
+  outputResult(currentResult, '');
 }
 
-add(1, 2);
-add(5, 10);
-add (20, 30);
-
-currentResult = ((currentResult + 10) * 3) / 2 - 1;
-let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
+addBtn.addEventListener('click', add);
 
 
-outputResult(currentResult, errorMessage);
