03a335e65779ec48635d1d42d770f4a682445a6c
Mateusz <mpodolski1988@gmail.com>
2025-10-13 23:32:10 +0200

    change add funcktion and add getUserInput function

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,9 +2,15 @@ const defaultResult = 0;
 
 let currentResult = defaultResult;
 
+function getUserInput () {
+  return parseInt(userInput.value);
+}
+
 function add(num1, num2) {
-  currentResult += userInput.value; 
-  outputResult(currentResult, '');
+  const enteredNumber = getUserInput();
+  const calcDescription = `${currentResult} + ${enteredNumber}`
+  currentResult += parseInt(enteredNumber); 
+  outputResult(currentResult, calcDescription);
 }
 
 addBtn.addEventListener('click', add);
