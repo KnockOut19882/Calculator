ba381dddeb51faba303009797cbfc4a0a5e5af87
Mateusz <mpodolski1988@gmail.com>
2025-10-14 12:13:07 +0200

    add new functions and addEventsLiteners

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,17 +2,44 @@ const defaultResult = 0;
 
 let currentResult = defaultResult;
 
-function getUserInput () {
+function getUserInput() {
   return parseInt(userInput.value);
+}
+
+function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
+  const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
+  outputResult(currentResult, calcDescription);
 }
 
 function add(num1, num2) {
   const enteredNumber = getUserInput();
-  const calcDescription = `${currentResult} + ${enteredNumber}`
-  currentResult += parseInt(enteredNumber); 
-  outputResult(currentResult, calcDescription);
+  const initialNumber = currentResult;
+  currentResult += enteredNumber;
+  createAndWriteOutput("+", initialNumber, enteredNumber);
 }
 
-addBtn.addEventListener('click', add);
+function subtract() {
+  const enteredNumber = getUserInput();
+  const initialNumber = currentResult;
+  currentResult -= enteredNumber;
+  createAndWriteOutput("-", initialNumber, enteredNumber);
+}
 
+function divide() {
+  const enteredNumber = getUserInput();
+  const initialNumber = currentResult;
+  currentResult /= enteredNumber;
+  createAndWriteOutput("/", initialNumber, enteredNumber);
+}
 
+function multiply() {
+  const enteredNumber = getUserInput();
+  const initialNumber = currentResult;
+  currentResult *= enteredNumber;
+  createAndWriteOutput("*", initialNumber, enteredNumber);
+}
+
+addBtn.addEventListener("click", add);
+subtractBtn.addEventListener("click", subtract);
+divideBtn.addEventListener("click", divide);
+multiplyBtn.addEventListener("click", multiply);
